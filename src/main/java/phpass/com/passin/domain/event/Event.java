77aad6419e2e0c10606b5739075c2a921a6af791
Pedro Henrique <pedro2266809ph@gmail.com>
2024-04-01 21:53:22 -0300

    feat: adding entity Event

---
+++ src/main/java/phpass/com/passin/domain/event/Event.java
@@ -0,0 +1,31 @@
+package phpass.com.passin.domain.event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "events")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Event {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String details;
+    @Column(nullable = false, unique = true)
+    private String slug;
+    @Column(nullable = false, name = "maximum_attendees")
+    private Integer maximumAttendees;
+
+
+}
