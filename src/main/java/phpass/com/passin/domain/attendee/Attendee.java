b74068a828e4e4d6a85b4d94ef40afeb27122976
Pedro Henrique <pedro2266809ph@gmail.com>
2024-04-01 22:00:04 -0300

    feat: adding entity Attendee doing ORM

---
+++ src/main/java/phpass/com/passin/domain/attendee/Attendee.java
@@ -0,0 +1,35 @@
+package phpass.com.passin.domain.attendee;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import phpass.com.passin.domain.event.Event;
+
+import java.time.LocalDateTime;
+
+@Table(name = "attendees")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Attendee {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false, name = "event_id")
+    private String eventId;
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
