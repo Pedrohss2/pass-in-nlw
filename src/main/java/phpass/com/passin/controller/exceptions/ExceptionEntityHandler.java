46757a2ec2b24cdfba59847aaf5ebd6f6717a495
Pedro Henrique <pedro2266809ph@gmail.com>
2024-04-03 23:51:07 -0300

    feat: putting more error methods in the ExceptionEntityHandler class

--- src/main/java/phpass/com/passin/controller/exceptions/ExceptionEntityHandler.java
+++ src/main/java/phpass/com/passin/controller/exceptions/ExceptionEntityHandler.java
@@ -1,10 +1,15 @@
 package phpass.com.passin.controller.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import phpass.com.passin.domain.attendee.exceptions.AttendeeAlreadyRegisteredException;
 import phpass.com.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import phpass.com.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import phpass.com.passin.domain.event.exceptions.EventFullException;
 import phpass.com.passin.domain.event.exceptions.EventNotFoundException;
+import phpass.com.passin.dto.general.ErrorResponseDTO;
 
 @ControllerAdvice
 public class ExceptionEntityHandler {
@@ -14,12 +19,25 @@ public class ExceptionEntityHandler {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity handlerEventFullException(EventFullException eventFullException) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(eventFullException.getMessage()));
+    }
+
     @ExceptionHandler(AttendeeNotFoundException.class)
     public ResponseEntity handleAttendeeNotFound(AttendeeNotFoundException attendeeNotFoundException) {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(AttendeeAlreadyRegisteredException.class)
+    public ResponseEntity handleAttendeeAlreadyRegisteredException(AttendeeAlreadyRegisteredException attendeeAlreadyRegisteredException) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
 
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handleCheckInAlreadyExistsException(CheckInAlreadyExistsException checkInAlreadyExistsException) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
 
 
 }
---
+++ src/main/java/phpass/com/passin/dto/general/ErrorResponseDTO.java
@@ -0,0 +1,4 @@
+package phpass.com.passin.dto.general;
+
+public record ErrorResponseDTO(String message) {
+}
